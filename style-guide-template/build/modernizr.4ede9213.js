b04633a148dd654c0207649e4bad1bdd72cb9fdb
Jeremy Hardman Account <jhardma4@byui.edu>
2019-05-28 22:16:22 -0600

    moved some stuff and adjusted other stuff

--- style-guide-template/build/modernizr.4ede9213.js
+++ style-guide-template/build/modernizr.4ede9213.js
@@ -729,7 +729,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54172" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61090" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
